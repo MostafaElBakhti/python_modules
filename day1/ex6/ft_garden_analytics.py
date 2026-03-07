00ae7ab5ae739c3bb609b8d07bb7df4fa72a58e4
mostafa <mustmustapha67@gmail.com>
2026-03-07 22:21:28 +0000

    improve code

--- day1/ex6/ft_garden_analytics.py
+++ day1/ex6/ft_garden_analytics.py
@@ -1,16 +1,21 @@
 class Plant:
-    def __init__(self, name: str, height: int, age: int) -> None:
+    def __init__(self,name: str, height: int) -> None:
         self.name: str = name
         self.height: int = height
-        self.age: int = age
+        # self.age: int = age
+        self.test: str = name
+    def grow(self) -> None :
+        self.height += 1
+        print(f"{self.name} grew 1cm")
 
-class FloweringPlant:
-    def __init__(self,name: str, height: int, age: int) -> None:
-        super().__init__(name, height, age)
+class FloweringPlant(Plant):
+    def __init__(self, name: str, height: int, color: str)-> None:
+        super().__init__(name,height)
+        self.color = color
 
-class PrizeFlower:
-    def __init__(self,name: str, height: int, age: int) -> None:
-        super().__init__(name, height, age)
+    def bloom(self)-> None:
+        print(f"{self.name} is blooming")
 
-class GardenManager:
-    
+class PrizeFlower(FloatingPointError):
+    def __init__(self, name: str, height: int, color: str, points : int):
+        super().__init__()
