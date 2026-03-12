47499e37a5dd29809b120195044d5cc485f314cf
mostafa <mustmustapha67@gmail.com>
2026-03-12 16:27:32 +0000

    improve code

--- day1/ex6/ft_garden_analytics.py
+++ day1/ex6/ft_garden_analytics.py
@@ -16,7 +16,7 @@ class FloweringPlant(Plant):
     def bloom(self)-> None:
         print(f"{self.name} is blooming")
 
-class PrizeFlower(FloatingPointError):
+class PrizeFlower(FloweringPlant):
     def __init__(self, name: str, height: int, color: str, points : int):
         super().__init__(name,height,color)
         self.points: int = points
@@ -26,10 +26,35 @@ class GardenManager:
         self.owner: str = owner
         self.plants: list[Plant] = []
 
+    def add_plant(self,plant: Plant) -> None:
+        self.plants.append(plant)
+        print(f"Added {plant.name} to {self.owner}'s garden")
+    
+    def help_plants_grow(self)->None:
+        print(f"{self.owner} is helping all plants grow...")
+        for plant in self.plants:
+            plant.grow()
+
+    def show_report(self)->None : 
+        print(f"=== {self.owner}'s Garden Report ===")
+        print("Plants in garden:")
+        for plant in self.plants:
+            if isinstance(plant,PrizeFlower):
+                print(f"- {plant.name}: {plant.height}cm, {plant.color} flowers (blooming), Prize points: {plant.points}")
+            elif isinstance(plant,FloweringPlant):
+                print(f"- {plant.name}: {plant.height}cm, {plant.color} flowers (blooming)")
+            else:
+                print(f"- {plant.name} Tree: {plant.height}cm")
+            
+
+
 if __name__ == "__main__":
     tree : Plant = Plant("Oak", 500)
     flower : FloweringPlant = FloweringPlant("Rose", 25, "red")
     prize_flower : PrizeFlower = PrizeFlower("Sunflower", 30, "yellow", 10)
-    tree.grow()
-    flower.grow()
+    alice  = GardenManager("Alice")
     # prize_flower.grow()
+    alice.add_plant(tree)
+    alice.add_plant(flower)
+    alice.help_plants_grow()
+    alice.show_report()
