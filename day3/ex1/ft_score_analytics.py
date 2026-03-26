3008a7de52bbae490f4ada2ca76b059cee36dcc6
mostafa <mustmustapha67@gmail.com>
2026-03-26 19:24:21 +0100

    improve code

---
+++ day3/ex1/ft_score_analytics.py
@@ -0,0 +1,28 @@
+import sys
+
+usage = "No scores provided. Usage: python3 " \
+        "ft_score_analytics.py <score1> <score2> ..."
+
+
+print("=== Player Score Analytics ===")
+if len(sys.argv) == 1:
+    print(usage)
+else:
+    scores = []
+
+    for i in range(1, len(sys.argv)):
+        try:
+            score = int(sys.argv[i])
+            scores = scores + [score]
+        except ValueError:
+            print(f"Invalid parameter: '{sys.argv[i]}'")
+    if len(scores) == 0:
+        print(usage)
+    else:
+        print(f"Scores processed: {scores}")
+        print(f"Total players: {len(scores)}")
+        print(f"Total score: {sum(scores)}")
+        print(f"Average score: {sum(scores) / len(scores)}")
+        print(f"High score: {max(scores)}")
+        print(f"Low score: {min(scores)}")
+        print(f"Score range: {max(scores) - min(scores)}")
