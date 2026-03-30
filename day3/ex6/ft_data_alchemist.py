58bf39e6c2fb6fa1350273d4a7525af158bcedcf
mostafa <mustmustapha67@gmail.com>
2026-03-30 14:20:00 +0100

    improve code

--- day3/ex6/ft_data_alchemist.py
+++ day3/ex6/ft_data_alchemist.py
@@ -6,13 +6,24 @@ names: list = [
     "Charlie",
     "dylan",
     "Emma",
-    "Gregory"
+    "Gregory",
+    "john",
+    "kevin",
+    "Liam"
     ]
 
 cap = [item.capitalize() for item in names]
-capitalized_names = [ item for item in names if item == item.capitalize()]
+capitalized_names = [item for item in names if item == item.capitalize()]
+scores = {item: random.randint(0, 1000) for item in cap}
+total = sum(scores.values())
+count = len(scores)
+average = round(total / count)
+high_score = {name: score for name, score in scores.items() if score > average}
 
 print("=== Game Data Alchemist ===")
 print(f"Initial list of players: {names}")
 print(f"New list with all names capitalized: {cap}")
 print(f"New list of capitalized names only: {capitalized_names}")
+print(f"Score dict : {scores}")
+print(f"Score average is {average}")
+print(f"High scores: {high_score}")
